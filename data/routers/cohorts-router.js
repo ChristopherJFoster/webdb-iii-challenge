383f4af16cb8abfefb4bd76682607eb935be5790
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-27 18:00:14 -0400

    POST cohort works. I'm trying to avoid copy/pasting everything, so I worked hard to figure out how to return the created record.

--- data/routers/cohorts-router.js
+++ data/routers/cohorts-router.js
@@ -2,6 +2,35 @@ const db = require('../dbConfig.js');
 
 const router = require('express').Router();
 
+router.post('/', async (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({
+      error: 'Please provide a cohort name.'
+    });
+  } else {
+    try {
+      const addedCohort = await db('cohorts').insert(req.body);
+      console.log(addedCohort);
+      try {
+        const newCohort = await db('cohorts')
+          .where('id', addedCohort[0])
+          .first();
+        console.log(newCohort);
+        res.status(201).json(newCohort);
+      } catch (err) {
+        res.status(500).json({
+          error: `There was an error while retrieving the added cohort data. ${err}`
+        });
+      }
+    } catch (err) {
+      res.status(500).json({
+        error: `There was an error while adding the cohort data. ${err}`
+      });
+    }
+  }
+});
+
 router.get('/', async (req, res) => {
   try {
     const cohorts = await db('cohorts');
