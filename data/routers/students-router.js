447561104bcac0d37ef99fa196205eed2e3649ed
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-28 10:14:03 -0400

    PUT student now works. If cohort_id is supplied, I check to see if it exists. I added the same check to POST student.

--- data/routers/students-router.js
+++ data/routers/students-router.js
@@ -3,29 +3,44 @@ const db = require('../dbConfig.js');
 const router = require('express').Router();
 
 router.post('/', async (req, res) => {
-  const { name } = req.body;
+  const { name, cohort_id } = req.body;
   if (!name) {
-    res.status(400).json({
+    return res.status(400).json({
       error: 'Please provide a student name.'
     });
-  } else {
+  } else if (cohort_id) {
     try {
-      const addedStudent = await db('students').insert(req.body);
-      try {
-        const addedRecord = await db('students')
-          .where('id', addedStudent[0])
-          .first();
-        res.status(201).json(addedRecord);
-      } catch (err) {
-        res.status(500).json({
-          error: `There was an error while retrieving the added student data. ${err}`
+      const cohort = await db('cohorts')
+        .where({ id: cohort_id })
+        .first();
+      if (!cohort) {
+        return res.status(400).json({
+          error: 'There is no cohort with the specified ID.'
         });
       }
     } catch (err) {
-      res.status(500).json({
-        error: `There was an error while adding the student data. ${err}`
+      return res.status(500).json({
+        error: `There was an error while checking the cohort data. ${err}`
       });
     }
+  }
+
+  try {
+    const addedStudent = await db('students').insert(req.body);
+    try {
+      const addedRecord = await db('students')
+        .where('id', addedStudent[0])
+        .first();
+      res.status(201).json(addedRecord);
+    } catch (err) {
+      res.status(500).json({
+        error: `There was an error while retrieving the added student data. ${err}`
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: `There was an error while adding the student data. ${err}`
+    });
   }
 });
 
@@ -66,6 +81,57 @@ router.get('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json({
       error: `There was an error while retrieving the student data. ${err}`
+    });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const id = req.params.id;
+  const { name, cohort_id } = req.body;
+  if (!name && !cohort_id) {
+    return res.status(400).json({
+      error: 'Please provide the student changes you intend to make.'
+    });
+  } else if (cohort_id) {
+    try {
+      const cohort = await db('cohorts')
+        .where({ id: cohort_id })
+        .first();
+      if (!cohort) {
+        return res.status(400).json({
+          error: 'There is no cohort with the specified ID.'
+        });
+      }
+    } catch (err) {
+      return res.status(500).json({
+        error: `There was an error while checking the cohort data. ${err}`
+      });
+    }
+  }
+
+  try {
+    const updatedStudent = await db('students')
+      .where({ id })
+      .update(req.body);
+    if (updatedStudent) {
+      try {
+        const updatedRecord = await db('students')
+          .where({ id })
+          .first();
+        res.status(200).json(updatedRecord);
+      } catch (err) {
+        res.status(500).json({
+          error: `There was an error while retrieving the updated student data. ${err}`
+        });
+      }
+    } else {
+      res.status(404).json({
+        message: 'There is no student with the specified ID.'
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: `There was an error while updating the student data. ${err}`
     });
   }
 });
